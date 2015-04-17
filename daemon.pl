8a3d06784679833ae346484cc2d3ebb64917fc3d
SWISH container user <J.Wielemaker@cs.vu.nl>
2015-04-17 10:34:55 +0200

    Updated location of log file

--- daemon.pl
+++ daemon.pl
@@ -1,4 +1,8 @@
-#!/usr/bin/env swipl
+#!/home/swish/bin/swipl
+
+:- use_module(library(settings)).
+:- use_module(library(http/http_log)).
+:- set_setting_default(http:logfile, 'log/httpd.log').
 
 :- use_module(library(http/http_unix_daemon)).
 :- initialization http_daemon.
